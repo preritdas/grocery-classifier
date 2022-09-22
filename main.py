bda95256fc2ca597033b61a5d6a78af55bfc9095
Prerit Das <preritdas@gmail.com>
2022-09-22 14:37:13 -0400

    Custom recipients.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from flask import Flask, request
 # Project
 import classification
 import texts
+import utils
 
 
 app = Flask(__name__)
@@ -22,9 +23,16 @@ def inbound_sms():
     grocery_list = inbound_sms_content["text"]
 
     texts.send_message(
-        content = classification.classify_grocery_list(grocery_list),
+        content = (result := classification.classify_grocery_list(grocery_list)),
         recipient = sender
     )
+
+    if sender in utils.CUSTOM_RECIPIENTS:
+        texts.send_message(
+            content = result,
+            recipient = utils.CUSTOM_RECIPIENTS[sender]
+        )
+
 
     return '', 204
 
