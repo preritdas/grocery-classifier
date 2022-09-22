935e75269a00de8f2c4e279e297c9e6cb60bafe4
Prerit Das <preritdas@gmail.com>
2022-09-22 14:00:20 -0400

    Read Nexmo keys.

---
+++ keys.py
@@ -0,0 +1,20 @@
+"""
+Parse keys into their appropriate types.
+"""
+import os
+import configparser
+
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+keys_path = os.path.join(current_dir, "keys.ini")
+
+keys = configparser.RawConfigParser()
+keys.read(keys_path)
+
+
+class Nexmo:
+    _nexmo_keys = keys["Nexmo"]
+    api_key = _nexmo_keys["api_key"]
+    api_secret = _nexmo_keys["api_secret"]
+    sender = _nexmo_keys["sender"]
+    mynumber = _nexmo_keys["mynumber"]
