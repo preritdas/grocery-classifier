ed1c4f664327c14429f204ddf9ca11965e8904d3
Prerit Das <preritdas@gmail.com>
2022-09-22 12:34:32 -0400

    Manual replacements.
    Sometimes the model doesn't work, ex. broccoli...

--- utils.py
+++ utils.py
@@ -1,8 +1,18 @@
 """
 Utils.
 """
+import json
+import os
+
 from pattern import en as pattern
 import nltk; nltk.download('omw-1.4', quiet=True, raise_on_error=True)
+
+
+# Load replacements
+current_dir = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(current_dir, "pluralization_replacements.json")) as f:
+    PLURAL_REPLACEMENTS: dict[str, str] = json.load(f)
+    SINGULAR_REPLACEMENTS = {val: key for key, val in PLURAL_REPLACEMENTS.items()}
 
 
 def pluralize(word: str):
@@ -10,7 +20,7 @@ def pluralize(word: str):
     Use the pattern library to smartly and correctly pluralize the word.
     """
     assert isinstance(word, str)
-    return pattern.pluralize(word)
+    return pattern.pluralize(word, custom=PLURAL_REPLACEMENTS)
 
 
 def singularize(word: str):
@@ -18,5 +28,5 @@ def singularize(word: str):
     Use the pattern library to smartly and correctly pluralize the word.
     """
     assert isinstance(word, str)
-    return pattern.singularize(word)
+    return pattern.singularize(word, SINGULAR_REPLACEMENTS)
  
