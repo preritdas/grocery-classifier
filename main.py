6c535de62607f12b9bff4ea9701119735e2ddb5d
Prerit Das <preritdas@gmail.com>
2022-09-22 22:25:38 -0400

    Proper tested payload handling.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Main.
 """
 # External
-from flask import Flask, request
+from flask import Flask, request, Response
 
 # Project
 import classification
@@ -20,29 +20,43 @@ def alert_use(number, content):
     )
 
 
-concats = {}
+payloads = {}
+
+
+@app.route('/', methods=["GET"])
+def inb():
+    print("Received.")
+    return Response(status=204)
 
 
 @app.route('/inbound-sms', methods=["POST"])
 def inbound_sms():
-    global concats
+    global payloads
 
     inbound_sms_content = request.get_json()
+    print("\n", inbound_sms_content, sep="")
+
     if type(inbound_sms_content) is not dict:
-        return ('', 400)
+        return Response(status=400)
+
+    # Payload handling
+    sender = inbound_sms_content["msisdn"]
+    if sender in payloads: payloads[sender].append(inbound_sms_content)
+    else: payloads[sender] = [inbound_sms_content]
 
     if "concat" in inbound_sms_content and inbound_sms_content["concat"] == "true":
-        if int(inbound_sms_content["concat-part"]) < int(inbound_sms_content["concat-total"]):
-            concats[inbound_sms_content["msisdn"]] = [inbound_sms_content["text"]]
-            print("Storing.")
-            return '', 204
-
-        # If this is the final concat message
-        print("Reading.")
-        inbound_sms_content["text"] = ''.join(concats[inbound_sms_content["msisdn"]] + [inbound_sms_content["text"]])
-        del concats[inbound_sms_content["msisdn"]]
+        required_concats = list(range(1, int(inbound_sms_content["concat-total"]) + 1))
+        present_concats = [int(message["concat-part"]) for message in payloads[sender]]
+        all_present = present_concats == required_concats
+        print(f"{all_present = }")
+        
+        if not all_present:  # not all messages stored
+            return Response(status=204)
+        
+        # All are present, construct the grocery list
+        messages = [message["text"] for message in payloads[sender]]
+        inbound_sms_content["text"] = ''.join(messages)
             
-    sender = inbound_sms_content["msisdn"]
     grocery_list = inbound_sms_content["text"]
 
     texts.send_message(
@@ -56,10 +70,14 @@ def inbound_sms():
             recipient = utils.CUSTOM_RECIPIENTS[sender]
         )
 
+    # Temporary usage alerts
     alert_use(sender, result)
 
-    print("Concats:", concats)
-    return '', 204
+    # Payload handling
+    del payloads[sender]
+    print("Payloads:", payloads)
+
+    return Response(status=204)
 
 
 if __name__ == '__main__':
