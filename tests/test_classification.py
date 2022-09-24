bfc378873893020d1882250eae24a18b40e1a667
Prerit Das <preritdas@gmail.com>
2022-09-24 00:15:17 -0400

    Test classification on custom setups.

--- tests/test_classification.py
+++ tests/test_classification.py
@@ -1,4 +1,5 @@
 import classification
+import utils
 
 
 GROCERY_LIST_TEST = """
@@ -15,5 +16,15 @@ GROCERY_LIST_TEST = """
 
 
 def test_classification():
-    assert classification.classify_grocery_list(GROCERY_LIST_TEST)
-    
+   assert classification.classify_grocery_list(GROCERY_LIST_TEST)
+
+   
+def test_custom_mappings():
+   for setup in utils.SETUPS:
+      lst = classification._classify_items(GROCERY_LIST_TEST, setup=setup)
+      assert lst
+      
+      # Check the order
+      order = utils.SETUPS["Whole Foods"][:] + ["none"]
+      order = [key for key in lst.keys() if key in order[:]]
+      assert order == list(lst.keys())
